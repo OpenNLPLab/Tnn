2e83b685c5f5750c9d324ad4928c1f41d8d09dee
Doraemonzzz <715073608@qq.com>
2023-02-03 20:25:59 +0800

    fix data_dir bug

--- script.sh
+++ script.sh
@@ -1,3 +1,6 @@
 arch=tnn_v2_decay_99_pre
 name=name
-bash train.sh 8 $arch $name 0.0005 0.0 M3T 0.2
+# change to your data dir
+data_dir=$1
+
+bash train.sh 8 $arch $name 0.0005 0.0 M3T 0.2 $data_dir
